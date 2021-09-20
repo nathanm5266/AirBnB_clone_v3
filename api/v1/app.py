044556dcd557b5b3e91a01631c67d33aa8689aaf
root <vincentjayden49@gmail.com>
2021-09-20 13:11:48 +0000

    api and register blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
-from flask import Flask, escape, request
+
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+@app.teardown_appcontext
+def teardown():
+    """ closes the session"""
+    storage.close()
 
 
 if __name__ == "__maain__":
-    app.run(host=0.0.0.0, port=5000, debug=True)
+    app.run(host=0.0.0.0, port=5000, threaded=True, debug=True)
